e3a6312f0f22988006971c1343224ae9846b13ad
rexqu <junjiequ123@gmail.com>
2023-04-20 11:50:45 -0400

    fix instruction

--- src/component/InstructionPage/InstructionPage.js
+++ src/component/InstructionPage/InstructionPage.js
@@ -3,25 +3,26 @@ function InstructionPage() {
     <div>
       <h1>Instructions</h1>
       <ul>
+        <li>Step 1: click on connect to github and allow permissions</li>
         <li>
-          Step 1: go to leetcode.com and use inspect element to open up the
+          Step 2: go to leetcode.com and use inspect element to open up the
           network tab, click refresh, find leetcode.com and copy everything in
           headers
         </li>
         <li>
-          Step 2: paste the header into the textbox to obtain session and csrf
+          Step 3: paste the header into the textbox to obtain session and csrf
           token
         </li>
 
         <li>
-          Step 3: set a repo name and enter in your accessToken, click on create
+          Step 4: set a repo name and enter in your accessToken, click on create
           repo
         </li>
         <li>
-          Step 4: Nagivate to github repo -> Action -> find the workflow Sync
+          Step 5: Nagivate to github repo -> Action -> find the workflow Sync
           Leetcode
         </li>
-        <li>Step 5: Click Run Workflow</li>
+        <li>Step 6: Click Run Workflow</li>
       </ul>
     </div>
   );
